bc7364033937fefbfb0d4db58aabf6a139d49428
Xavier Vicient <xvicient@gmail.com>
2025-01-10 23:10:19 +0100

    Extracting GoogleSignIn, Firebase and Data module

---
+++ Packages/External/FirebaseDependencies/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FirebaseDependencies",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FirebaseDependencies",
+            targets: ["FirebaseDependencies"]),
+    ],
+    dependencies: [
+        .package(url: "https://www.github.com/firebase/firebase-ios-sdk.git", from: "11.6.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FirebaseDependencies",
+            dependencies: [
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+            ]
+        )
+    ]
+)
