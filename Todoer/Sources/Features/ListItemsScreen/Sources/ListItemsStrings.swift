5803191b504631d8da733f1909dc21a4304efff4
Xavier Vicient <xavier.vicient@free-now.com>
2025-01-21 11:30:11 +0100

    decoupling alert from listitems

---
+++ Todoer/Sources/Features/ListItemsScreen/Sources/ListItemsStrings.swift
@@ -0,0 +1,10 @@
+// MARK: - Constants
+
+public extension ListItems {
+    struct Strings {
+        static let errorTitle = "Error"
+        static let errorOkButton = "Ok"
+        static let newRowButtonTitle = "New Item"
+        static let sortButtonTitle = "Sort"
+    }
+}
