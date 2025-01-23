7ab4093da588c7fc1d00b118f62b93221a52e97b
Xavier Vicient <xvicient@gmail.com>
2025-01-23 10:50:33 +0100

    Documenting about screen

--- Todoer/Sources/Features/AboutScreen/Sources/AboutBuilder.swift
+++ Todoer/Sources/Features/AboutScreen/Sources/AboutBuilder.swift
@@ -1,7 +1,13 @@
 import SwiftUI
 
 public struct About {
+    
+    /// Builder for creating and configuring About screen instances
     public struct Builder {
+        
+        /// Creates a new About screen instance
+        /// - Parameters:
+        /// - Returns: Configured About screen view
 		@MainActor
         public static func makeAbout() -> some View {
 			AboutScreen()
